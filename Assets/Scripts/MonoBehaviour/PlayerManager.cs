fbb21518bc7883d3238a1914a5e2c9289c9caeb9
JPillS <jason.pilla@student.sl.on.ca>
2025-10-26 19:27:24 -0400

    Added methods InitializePlayer and InstantiateCards.

--- Assets/Scripts/MonoBehaviour/PlayerManager.cs
+++ Assets/Scripts/MonoBehaviour/PlayerManager.cs
@@ -4,16 +4,40 @@ namespace TexasHoldem.MonoScripts
 {
     public class PlayerManager : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        // non-static members
+        // fields
+        [SerializeField] Player player;
+        
+        // unity messages
         void Start()
+        {
+            InstantiateCards();
+        }
+
+        void Update()
         {
 
         }
 
-        // Update is called once per frame
-        void Update()
+        // methods
+        /// <summary>
+        /// initializes player object
+        /// </summary>
+        /// <param name="name"></param>
+        public void InitializePlayer(string name)
         {
+            player = new Player(name);
+        }
 
+        public void InstantiateCards()
+        {
+            Vector3 cardOffset = new(.2f, -.02f);
+            for (int index = 0; index < Hole.SIZE; index++)
+            {
+                GameObject card = Instantiate(player.Cards[index].CardPrefab, transform);
+                card.transform.localPosition += index == 0 ? cardOffset : -cardOffset;
+                card.GetComponent<SpriteRenderer>().sortingOrder = index == 0 ? 1 : 0;
+            }
         }
     }
 }
