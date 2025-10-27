f197975c39abc383b0cfb19ffeb759cc1fd46167
JPillS <jason.pilla@student.sl.on.ca>
2025-10-26 20:58:43 -0400

    Moved things around, added Player property, used for instead of foreach for InstantiateCards to offset the game objects, added DestroyCards() method.

--- Assets/Scripts/MonoBehaviour/PlayerManager.cs
+++ Assets/Scripts/MonoBehaviour/PlayerManager.cs
@@ -7,11 +7,15 @@ namespace TexasHoldem.MonoScripts
         // non-static members
         // fields
         [SerializeField] Player player;
+        GameObject[] cards = new GameObject[Hole.SIZE];
+
+        // properties
+        public Player Player { get { return player; } }
         
         // unity messages
         void Start()
         {
-            InstantiateCards();
+
         }
 
         void Update()
@@ -34,10 +38,16 @@ namespace TexasHoldem.MonoScripts
             Vector3 cardOffset = new(.2f, -.02f);
             for (int index = 0; index < Hole.SIZE; index++)
             {
-                GameObject card = Instantiate(player.Cards[index].CardPrefab, transform);
-                card.transform.localPosition += index == 0 ? cardOffset : -cardOffset;
-                card.GetComponent<SpriteRenderer>().sortingOrder = index == 0 ? 1 : 0;
+                cards[index] = Instantiate(player.Cards[index].CardPrefab, transform);
+                cards[index].transform.localPosition += index == 0 ? cardOffset : -cardOffset;
+                cards[index].GetComponent<SpriteRenderer>().sortingOrder = index == 0 ? 1 : 0;
             }
+        }
+
+        public void DesroyCards()
+        {
+            foreach (GameObject card in cards)
+                Destroy(card);
         }
     }
 }
