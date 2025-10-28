a53cf89c650e76e3fe1794373e60882810b5327e
JPillS <jason.pilla@student.sl.on.ca>
2025-10-28 16:33:50 -0400

    Added field and property state and State and method NextState to static Game, and moved enum State outside the class. Also removed OpeningDeal state.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,22 +3,40 @@ namespace TexasHoldem
     public static class Game
     {
         // static members
+        // fields
+        static State state = State.Start;
+
         // constants
         public const int BOARD_SIZE = 5;
         public const int HAND_SIZE = BOARD_SIZE + Hole.SIZE;
 
-        // enums
-        /// <summary>
-        /// game states
-        /// </summary>
-        public enum State
+        // properties
+        public static State State
         {
-            Start,
-            OpeningDeal,
-            RoundStart,
-            Deal,
-            Flip,
-            NextRound
+            get { return state; }
+            set
+            {
+                state = value < State.NextRound ? value : state = State.Start;
+            }
         }
+
+        // methods
+        public static void NextState()
+        {
+            State++;
+        }
+    }
+
+    // enums
+    /// <summary>
+    /// game states
+    /// </summary>
+    public enum State
+    {
+        Start,
+        RoundStart,
+        Deal,
+        Flip,
+        NextRound
     }
 }
